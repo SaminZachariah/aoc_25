c6247c4ccdec92e3a02e320e04ee00cfde923527
Samin Zachariah <saminzachariah@gmail.com>
2025-12-04 20:54:30 -0800

    Switch to Set for performance.

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import AoCUtils
+import Data.Set qualified as Set
 import System.Environment (getArgs)
 
 main :: IO ()
@@ -21,43 +22,37 @@ main = do
   putStrLn $ "Part 2: " ++ show part2
 
 solve1 :: [String] -> Int
-solve1 input =
-  let rolls = locateRolls input
-      neighborCounts = map (`countNeighbors` rolls) rolls
-      accessibleRolls = filter (< 4) neighborCounts
-   in length accessibleRolls
+solve1 input = length accessibleRolls
+  where
+    rolls = locateRolls input
+    accessibleRolls = filter (< 4) $ map (`countNeighbors` rolls) (Set.toList rolls)
 
 solve2 :: [String] -> Int
 solve2 _ = 0
 
--- | locateRolls returns a list of position-tuples, for every place a roll
--- | appears in the puzzle input
--- | [..., (x-pos, y-pos), ...] (0 indexed positions)
--- | 1st generator zips (y-idx-N, input-line-N) for each input line
--- | 2nd generator zips (x-pos-M, input-char-M) for each char in a line
--- | The 2nd generator gets the 'row' bound in the 1st generator
--- | and acts like a nested loop
--- | 'isRollChar char' is a boolean test to filter out non-roll characters
-locateRolls :: [String] -> [(Int, Int)]
+-- | locateRolls returns position-tuples, for every place a roll
+-- | appears in the puzzle input (0 indexed positions)
+-- | List-comp generates all (x,y) pairs that meet `isRollChar char` predicate
+locateRolls :: [String] -> Set.Set (Int, Int)
 locateRolls input =
-  [ (x, y)
-    | (y, row) <- zip [0 ..] input,
-      (x, char) <- zip [0 ..] row,
-      isRollChar char
-  ]
+  Set.fromList
+    [ (x, y)
+      | (y, row) <- zip [0 ..] input,
+        (x, char) <- zip [0 ..] row,
+        isRollChar char
+    ]
 
--- | countNeighbors takes a a position, and the list of rolls,
+-- | countNeighbors takes a a position, and the collection of rolls,
 -- | and returns the number of neighboring rolls
-countNeighbors :: (Int, Int) -> [(Int, Int)] -> Int
+countNeighbors :: (Int, Int) -> Set.Set (Int, Int) -> Int
 countNeighbors (x, y) rolls =
-  let testPositions =
-        [ (x', y')
-          | x' <- [x - 1 .. x + 1],
-            y' <- [y - 1 .. y + 1],
-            (x', y') /= (x, y)
-        ]
-      neighborRolls = filter (`elem` rolls) testPositions
-   in length neighborRolls
+  length
+    [ (x', y')
+      | x' <- [x - 1 .. x + 1],
+        y' <- [y - 1 .. y + 1],
+        (x', y') /= (x, y),
+        Set.member (x', y') rolls
+    ]
 
 isRollChar :: Char -> Bool
 isRollChar '@' = True
